e68e54224c7e91b8c4c86826dbbcd65c36240d94
Aliaksandr Konan <newalex1985@tut.by>
2019-09-29 22:25:27 +0300

    feat: add solution of task

--- src/index.js
+++ src/index.js
@@ -1,44 +1,63 @@
 const MORSE_TABLE = {
-    '.-':     'a',
-    '-...':   'b',
-    '-.-.':   'c',
-    '-..':    'd',
-    '.':      'e',
-    '..-.':   'f',
-    '--.':    'g',
-    '....':   'h',
-    '..':     'i',
-    '.---':   'j',
-    '-.-':    'k',
-    '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
+    '.-': 'a',
+    '-...': 'b',
+    '-.-.': 'c',
+    '-..': 'd',
+    '.': 'e',
+    '..-.': 'f',
+    '--.': 'g',
+    '....': 'h',
+    '..': 'i',
+    '.---': 'j',
+    '-.-': 'k',
+    '.-..': 'l',
+    '--': 'm',
+    '-.': 'n',
+    '---': 'o',
+    '.--.': 'p',
+    '--.-': 'q',
+    '.-.': 'r',
+    '...': 's',
+    '-': 't',
+    '..-': 'u',
+    '...-': 'v',
+    '.--': 'w',
+    '-..-': 'x',
+    '-.--': 'y',
+    '--..': 'z',
+    '.----': '1',
+    '..---': '2',
+    '...--': '3',
+    '....-': '4',
+    '.....': '5',
+    '-....': '6',
+    '--...': '7',
+    '---..': '8',
+    '----.': '9',
+    '-----': '0',
 };
 
 function decode(expr) {
-    // write your solution here
+
+    let expression = '';
+    const letterLength = 10;
+    for (let i = 0; i < expr.length / letterLength; i += 1) {
+        let letter = '';
+        let letterString = expr.slice(i * letterLength, letterLength * (i + 1));
+        for (let j = 0; j < letterLength; j += 2) {
+            if ((letterString[j] === '1') && (letterString[j + 1] === '0')) {
+                letter += '.';
+            } else if ((letterString[j] === '1') && (letterString[j + 1] === '1')) {
+                letter += '-';
+            } else if ((letterString[j] === '*') && (letterString[j + 1] === '*')) {
+                letter = 'space';
+                break;
+            }
+        }
+        expression += (letter === 'space') ? ' ' : MORSE_TABLE[letter];
+    }
+    return expression;
+
 }
 
 module.exports = {
